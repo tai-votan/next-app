6977f1bafa842c5146aa9af74e108f281d94516c
votantai <taivo.it2210@gmail.com>
2021-01-07 00:36:47 +0700

    remove package next-purgecss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const withSass = require("@zeit/next-sass");
 const withLess = require("@zeit/next-less");
 const lessToJS = require("less-vars-to-js");
-const withPurgeCss = require("next-purgecss");
 const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
 const AntdDayjsWebpackPlugin = require("antd-dayjs-webpack-plugin");
 
@@ -17,48 +17,46 @@ const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, "./style
 module.exports = withBundleAnalyzer(
   withSass({
     cssModules: true,
-    ...withLess(
-      withPurgeCss({
-        lessLoaderOptions: {
-          javascriptEnabled: true,
-          modifyVars: themeVariables // make your antd custom effective
-        },
-        webpack: (config, { isServer }) => {
-          if (isServer) {
-            const antStyles = /antd\/.*?\/style.*?/;
-            const origExternals = [...config.externals];
-            // eslint-disable-next-line no-param-reassign
-            config.externals = [
-              // eslint-disable-next-line consistent-return
-              (context, request, callback) => {
-                if (request.match(antStyles)) return callback();
-                if (typeof origExternals[0] === "function") {
-                  origExternals[0](context, request, callback);
-                } else {
-                  callback();
-                }
-              },
-              ...(typeof origExternals[0] === "function" ? [] : origExternals)
-            ];
+    ...withLess({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables // make your antd custom effective
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          // eslint-disable-next-line no-param-reassign
+          config.externals = [
+            // eslint-disable-next-line consistent-return
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === "function") {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === "function" ? [] : origExternals)
+          ];
 
-            config.module.rules.unshift({
-              test: antStyles,
-              use: "null-loader"
-            });
-          }
-
-          config.plugins.push(
-            new AntdDayjsWebpackPlugin(),
-            new FilterWarningsPlugin({
-              exclude: /mini-css-extract-plugin[^]*Conflicting order between:/
-            })
-          );
-
-          config.resolve.modules.push(__dirname);
-
-          return config;
+          config.module.rules.unshift({
+            test: antStyles,
+            use: "null-loader"
+          });
         }
-      })
-    )
+
+        config.plugins.push(
+          new AntdDayjsWebpackPlugin(),
+          new FilterWarningsPlugin({
+            exclude: /mini-css-extract-plugin[^]*Conflicting order between:/
+          })
+        );
+
+        config.resolve.modules.push(__dirname);
+
+        return config;
+      }
+    })
   })
 );
