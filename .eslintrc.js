ffd9fd9a3c46eb40b4cdfa3478c84d9e9622a9c3
tai.votan <taivo.it2210@gmail.com>
2021-03-04 00:48:13 +0700

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     ecmaVersion: 2020, // Use the latest ecmascript standard
     sourceType: "module", // Allows using import/export statements
     ecmaFeatures: {
-      jsx: true // Enable JSX since we're using React
+      jsx: true, // Enable JSX since we're using React
+      modules: true
     }
   },
   settings: {
@@ -15,14 +16,17 @@ module.exports = {
   env: {
     browser: true, // Enables browser globals like window and document
     amd: true, // Enables require() and define() as global variables as per the amd spec.
-    node: true // Enables Node.js global variables and Node.js scoping.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+    jest: true
   },
   extends: [
     require.resolve("@umijs/fabric/dist/eslint"),
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended" // Make this the last element so prettier config overrides other formatting rules
+    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   rules: {
     "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
---
+++ pages/login.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { mount } from 'enzyme';
+import Login from './login';
+
+describe('Login Component', () => {
+  it('should render without crashing', () => {
+    const wrapper = mount(<Login />);
+    expect(wrapper.find('a')).toHaveLength(1);
+  });
+});
